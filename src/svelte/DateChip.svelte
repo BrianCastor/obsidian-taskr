45a9b65231bd53cfbfc254427fade78ed9b3012e
BrianCastor <16365884+BrianCastor@users.noreply.github.com>
2023-01-12 02:32:38 -0500

    Better date picker options

--- src/svelte/DateChip.svelte
+++ src/svelte/DateChip.svelte
@@ -4,36 +4,71 @@
         isToday,
         isTomorrow,
         addDays,
-        parse,
-        format
+        endOfWeek,
+        eachDayOfInterval,
+        format,
+        startOfWeek,
+        isWeekend,
+        endOfMonth,
+        addMonths,
     } from "date-fns";
     import { formatDateRelativeToNow } from "../date_utils";
-    import {showDropdownMenu} from "../components/DropdownMenu";
+    import { showDropdownMenu } from "../components/DropdownMenu";
     import CalendarPopup from "./CalendarPopup.svelte";
     import { Modal } from "obsidian";
-    import {DatePicker} from 'date-picker-svelte';
-
 
     export let date: Date | undefined;
     export let setDate: (date: Date | undefined) => void;
     export let emoji: string;
     export let complete: boolean | undefined = false;
-    export let size: ("small" | "normal" | "large") = "small"
+    export let size: "small" | "normal" | "large" = "small";
 
     let container: HTMLDivElement;
-    let showCalendar: boolean = false;
     let dateOptions = [
-        { label: "Today", onClick: () => (setDate(new Date()))},
-        { label: "Tomorrow", onClick: () => (setDate(addDays(new Date(), 1))) },
-        { label: "+2 Days", onClick: () => (setDate(addDays(new Date(), 2)))},
-        { label: "+3 Days", onClick: () => (setDate(addDays(new Date(), 2))) },
-        { label: "+1 week", onClick: () => (setDate(addDays(new Date(), 7))) },
-        { label: "+1 month", onClick: () => (setDate(addDays(new Date(), 30))) },
-        { label: "Other date", onClick: () => {
-            renderCalendar();
-        }},
-        { label: "Remove", onClick: () => (setDate(undefined))}
+        { label: "Today", onClick: () => setDate(new Date()) },
+        { label: "Tomorrow", onClick: () => setDate(addDays(new Date(), 1)) },
     ];
+
+    eachDayOfInterval({
+        start: addDays(new Date(), 2),
+        end: addDays(new Date(), 6),
+    }).map((date: Date) => {
+        dateOptions.push({
+            label: format(date, "EEEE"),
+            onClick: () => setDate(date),
+        });
+    });
+
+    if (!isWeekend(new Date())) {
+        dateOptions.push({
+            label: 'End of Week',
+            onClick: () => setDate(endOfWeek(new Date(), {weekStartsOn: 6}))
+        })
+    }
+
+    dateOptions.push({
+        label: 'End of Weekend',
+        onClick: () => setDate(endOfWeek(new Date(), {weekStartsOn: 1}))
+    })
+
+    if (!isWeekend(new Date())) {
+        dateOptions.push({
+            label: 'End of Next Week',
+            onClick: () => setDate(addDays(startOfWeek(new Date(), {weekStartsOn: 1}), 11))
+        })
+    }
+
+    dateOptions = dateOptions.concat([
+        { label: "End of Month", onClick: () => setDate(endOfMonth(new Date())) },
+        { label: "End of Next Month", onClick: () => setDate(endOfMonth(addMonths(new Date(), 1)))},
+        {
+            label: "Other date",
+            onClick: () => {
+                renderCalendar();
+            },
+        },
+        { label: "Remove", onClick: () => setDate(undefined) },
+    ]);
 
     const getColor = (dt: Date | undefined) => {
         if (!dt) return "grey";
@@ -43,35 +78,34 @@
         if (startOfDay(new Date()) > dt) return "rgb(250,45,45)";
         if (isToday(dt)) return "rgb(60,250,60)";
         if (isTomorrow(dt)) return "rgb(30,250,250)";
-        
+
         return "rgb(230,230,230)";
     };
 
     const renderCalendar = () => {
-        const modal = new Modal(app)
-        modal.containerEl.style.padding = '0px';
-        modal.containerEl.addClass('taskr-calendar-modal')
+        const modal = new Modal(app);
+        modal.containerEl.style.padding = "0px";
+        modal.containerEl.addClass("taskr-calendar-modal");
         modal.onOpen = () => {
             const { titleEl, contentEl } = modal;
             new CalendarPopup({
                 target: contentEl,
                 props: {
-                    date:date,
+                    date: date,
                     setDate: (dt) => {
-                        setDate(dt)
-                        modal.close()
-                    }
+                        setDate(dt);
+                        modal.close();
+                    },
                 },
-                
-            })
-        }
+            });
+        };
         modal.onClose = (): void => {
             const { contentEl } = modal;
             contentEl.empty();
         };
 
-        modal.open()
-    }
+        modal.open();
+    };
 </script>
 
 <div
@@ -100,25 +134,25 @@
         margin-right: 10px;
         padding: 0px 5px;
         cursor: pointer;
-        font-size:14px;
+        font-size: 14px;
     }
     .chip-container.small {
-        font-size:12px;
+        font-size: 12px;
     }
     .chip-container.large {
-        padding:5px 10px;
-        font-size:14px;
+        padding: 5px 10px;
+        font-size: 14px;
     }
 
     :global(.taskr-calendar-modal .page-button) {
-        padding:0px !important;
+        padding: 0px !important;
     }
 
     :global(.taskr-calendar-modal .dropdown) {
         height: 1.5rem !important;
         padding: 0px !important;
     }
-    
+
     :global(.taskr-calendar-modal .modal-close-button) {
         display: none !important;
     }
@@ -131,7 +165,7 @@
     :global(.taskr-calendar-modal) {
         --date-picker-background: #141414;
         --date-picker-foreground: white;
-        --date-picker-highlight-border: rgba(1,1,1,0);
+        --date-picker-highlight-border: rgba(1, 1, 1, 0);
         --date-picker-highlight-shadow: cyan;
         --date-picker-selected-color: white;
         --date-picker-selected-background: cyan;
